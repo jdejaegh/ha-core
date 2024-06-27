34a86f4d985fc8256aeb83d85cb2d3cde8b7c11d
Jules Dejaeghere <dej.j@live.be>
2024-06-27 20:43:35 +0200

    Add more pollutant to track

--- homeassistant/components/irceline/const.py
+++ homeassistant/components/irceline/const.py
@@ -1,5 +1,7 @@
 """Constants for the IRCEL - CELINE: Air quality Belgium integration."""
 
+from enum import StrEnum
+
 from open_irceline import RioFeature
 
 from homeassistant.components.sensor import SensorDeviceClass
@@ -11,22 +13,53 @@ PLATFORMS: list[Platform] = [Platform.SENSOR]
 IRCEL_CELINE = "Belgian Interregional Environment Agency (IRCEL - CELINE)"
 ATTRIBUTION = f"Air quality data from the {IRCEL_CELINE}"
 
-RIO_HOURLY_POLLUTANT = {"current_no2_hmean", "current_pm25_hmean", "current_o3_hmean"}
-
-POLLUTANT_TO_FEATURE = {
-    "current_no2_hmean": RioFeature.NO2_HMEAN,
-    "current_pm25_hmean": RioFeature.PM25_HMEAN,
-    "current_o3_hmean": RioFeature.O3_HMEAN,
+RIO_HOURLY_POLLUTANT = {
+    "current_bc_24hmean",
+    "current_bc_hmean",
+    "current_no2_hmean",
+    "current_o3_hmean",
+    "current_pm10_24hmean",
+    "current_pm10_hmean",
+    "current_pm25_24hmean",
+    "current_pm25_hmean",
 }
 
+POLLUTANT_TO_FEATURE = {
+    "current_bc_24hmean": RioFeature.BC_24HMEAN,
+    "current_bc_hmean": RioFeature.BC_HMEAN,
+    "current_no2_hmean": RioFeature.NO2_HMEAN,
+    "current_o3_hmean": RioFeature.O3_HMEAN,
+    "current_pm10_24hmean": RioFeature.PM10_24HMEAN,
+    "current_pm10_hmean": RioFeature.PM10_HMEAN,
+    "current_pm25_24hmean": RioFeature.PM25_24HMEAN,
+    "current_pm25_hmean": RioFeature.PM25_HMEAN,
+}
+
+
+class IrcelineSensorDeviceClass(StrEnum):
+    """Enum extending SensorDeviceClass to include some values that are needed for this integration."""
+
+    BC = "black_carbon"
+
+
 POLLUTANT_TO_SENSOR_DEVICE_CLASS = {
+    "current_bc_24hmean": IrcelineSensorDeviceClass.BC,
+    "current_bc_hmean": IrcelineSensorDeviceClass.BC,
     "current_no2_hmean": SensorDeviceClass.NITROGEN_DIOXIDE,
-    "current_pm25_hmean": SensorDeviceClass.PM25,
     "current_o3_hmean": SensorDeviceClass.OZONE,
+    "current_pm10_24hmean": SensorDeviceClass.PM10,
+    "current_pm10_hmean": SensorDeviceClass.PM10,
+    "current_pm25_24hmean": SensorDeviceClass.PM25,
+    "current_pm25_hmean": SensorDeviceClass.PM25,
 }
 
 POLLUTANT_TO_UNIT = {
+    "current_bc_24hmean": CONCENTRATION_MICROGRAMS_PER_CUBIC_METER,
+    "current_bc_hmean": CONCENTRATION_MICROGRAMS_PER_CUBIC_METER,
     "current_no2_hmean": CONCENTRATION_MICROGRAMS_PER_CUBIC_METER,
-    "current_pm25_hmean": CONCENTRATION_MICROGRAMS_PER_CUBIC_METER,
     "current_o3_hmean": CONCENTRATION_MICROGRAMS_PER_CUBIC_METER,
+    "current_pm10_24hmean": CONCENTRATION_MICROGRAMS_PER_CUBIC_METER,
+    "current_pm10_hmean": CONCENTRATION_MICROGRAMS_PER_CUBIC_METER,
+    "current_pm25_24hmean": CONCENTRATION_MICROGRAMS_PER_CUBIC_METER,
+    "current_pm25_hmean": CONCENTRATION_MICROGRAMS_PER_CUBIC_METER,
 }
--- tests/components/irceline/test_const.py
+++ tests/components/irceline/test_const.py
@@ -5,7 +5,9 @@ from homeassistant.components.irceline.const import (
     POLLUTANT_TO_SENSOR_DEVICE_CLASS,
     POLLUTANT_TO_UNIT,
     RIO_HOURLY_POLLUTANT,
+    IrcelineSensorDeviceClass,
 )
+from homeassistant.components.sensor import SensorDeviceClass
 
 
 def test_consistency_const():
@@ -15,3 +17,11 @@ def test_consistency_const():
         assert pollutant in POLLUTANT_TO_FEATURE
         assert pollutant in POLLUTANT_TO_SENSOR_DEVICE_CLASS
         assert pollutant in POLLUTANT_TO_UNIT
+
+
+def test_ensure_non_colliding_enums():
+    """Test that the values in SensorDeviceClass and IrcelineSensorDeviceClass are all different to avoid clashes."""
+    device_class_irceline = {str(v) for v in IrcelineSensorDeviceClass}
+    device_class_builtin = {str(v) for v in SensorDeviceClass}
+
+    assert not device_class_builtin.intersection(device_class_irceline)
