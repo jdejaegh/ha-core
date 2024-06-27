459e2558806b033d4f7ac83cc1990c7850fcdb63
Jules Dejaeghere <dej.j@live.be>
2024-06-27 22:18:18 +0200

    Refactor sensor classes and add forecast sensors

--- homeassistant/components/irceline/const.py
+++ homeassistant/components/irceline/const.py
@@ -1,8 +1,6 @@
 """Constants for the IRCEL - CELINE: Air quality Belgium integration."""
 
-from enum import StrEnum
-
-from open_irceline import RioFeature
+from open_irceline import ForecastFeature, RioFeature
 
 from homeassistant.components.sensor import SensorDeviceClass
 from homeassistant.const import CONCENTRATION_MICROGRAMS_PER_CUBIC_METER, Platform
@@ -24,6 +22,14 @@ RIO_HOURLY_POLLUTANT = {
     "current_pm25_hmean",
 }
 
+FORECAST_POLLUTANT = {
+    "forecast_no2_maxhmean",
+    "forecast_o3_maxhmean",
+    "forecast_pm10_dmean",
+    "forecast_pm25_dmean",
+}
+
+# noinspection DuplicatedCode
 POLLUTANT_TO_FEATURE = {
     "current_bc_24hmean": RioFeature.BC_24HMEAN,
     "current_bc_hmean": RioFeature.BC_HMEAN,
@@ -33,24 +39,31 @@ POLLUTANT_TO_FEATURE = {
     "current_pm10_hmean": RioFeature.PM10_HMEAN,
     "current_pm25_24hmean": RioFeature.PM25_24HMEAN,
     "current_pm25_hmean": RioFeature.PM25_HMEAN,
+    "forecast_no2_maxhmean": ForecastFeature.NO2_MAXHMEAN,
+    "forecast_o3_maxhmean": ForecastFeature.O3_MAXHMEAN,
+    "forecast_pm10_dmean": ForecastFeature.PM10_DMEAN,
+    "forecast_pm25_dmean": ForecastFeature.PM25_DMEAN,
 }
 
 
-class IrcelineSensorDeviceClass(StrEnum):
-    """Enum extending SensorDeviceClass to include some values that are needed for this integration."""
-
-    BC = "black_carbon"
-
-
+# noinspection DuplicatedCode
 POLLUTANT_TO_SENSOR_DEVICE_CLASS = {
-    "current_bc_24hmean": IrcelineSensorDeviceClass.BC,
-    "current_bc_hmean": IrcelineSensorDeviceClass.BC,
+    # Have to put None for the classes that are not available in SensorDeviceClass, else mypy linter complains when
+    # setting _attr_device_class in sensor __init__:
+    # > error: Incompatible types in assignment (expression has type "StrEnum", variable has type
+    # "SensorDeviceClass | None")  [assignment]
+    "current_bc_24hmean": None,
+    "current_bc_hmean": None,
     "current_no2_hmean": SensorDeviceClass.NITROGEN_DIOXIDE,
     "current_o3_hmean": SensorDeviceClass.OZONE,
     "current_pm10_24hmean": SensorDeviceClass.PM10,
     "current_pm10_hmean": SensorDeviceClass.PM10,
     "current_pm25_24hmean": SensorDeviceClass.PM25,
     "current_pm25_hmean": SensorDeviceClass.PM25,
+    "forecast_no2_maxhmean": SensorDeviceClass.NITROGEN_DIOXIDE,
+    "forecast_o3_maxhmean": SensorDeviceClass.OZONE,
+    "forecast_pm10_dmean": SensorDeviceClass.PM10,
+    "forecast_pm25_dmean": SensorDeviceClass.PM25,
 }
 
 POLLUTANT_TO_UNIT = {
@@ -62,4 +75,8 @@ POLLUTANT_TO_UNIT = {
     "current_pm10_hmean": CONCENTRATION_MICROGRAMS_PER_CUBIC_METER,
     "current_pm25_24hmean": CONCENTRATION_MICROGRAMS_PER_CUBIC_METER,
     "current_pm25_hmean": CONCENTRATION_MICROGRAMS_PER_CUBIC_METER,
+    "forecast_no2_maxhmean": CONCENTRATION_MICROGRAMS_PER_CUBIC_METER,
+    "forecast_o3_maxhmean": CONCENTRATION_MICROGRAMS_PER_CUBIC_METER,
+    "forecast_pm10_dmean": CONCENTRATION_MICROGRAMS_PER_CUBIC_METER,
+    "forecast_pm25_dmean": CONCENTRATION_MICROGRAMS_PER_CUBIC_METER,
 }
--- homeassistant/components/irceline/sensor.py
+++ homeassistant/components/irceline/sensor.py
@@ -1,6 +1,9 @@
 """Sensors for pollutants from IRCEL - CELINE."""
 
+from abc import ABCMeta, abstractmethod
 from collections.abc import Mapping
+from datetime import date, timedelta
+from itertools import product
 import logging
 from typing import Any
 
@@ -14,9 +17,11 @@ from homeassistant.helpers.update_coordinator import CoordinatorEntity
 from .const import (
     ATTRIBUTION,
     DOMAIN,
+    FORECAST_POLLUTANT,
     POLLUTANT_TO_FEATURE,
     POLLUTANT_TO_SENSOR_DEVICE_CLASS,
     POLLUTANT_TO_UNIT,
+    RIO_HOURLY_POLLUTANT,
 )
 from .coordinator import IrcelineCoordinator
 
@@ -28,47 +33,95 @@ async def async_setup_entry(
 ) -> None:
     """Set up the sensor platform."""
     coordinator = hass.data[DOMAIN][entry.entry_id]
+
     async_add_entities(
         [
             IrcelinePollutantRio(coordinator, entry, pollutant)
-            for pollutant in POLLUTANT_TO_FEATURE
+            for pollutant in RIO_HOURLY_POLLUTANT
+        ]
+    )
+
+    async_add_entities(
+        [
+            IrcelinePollutantForecast(coordinator, entry, pollutant, timedelta(days=d))
+            for pollutant, d in product(FORECAST_POLLUTANT, range(4))
         ]
     )
 
 
-class IrcelinePollutantRio(CoordinatorEntity, SensorEntity):
-    """Representation of a pollutant sensor with value extracted from the RIO interpolation of IRCELINE."""
+class IrcelinePollutantSensor(CoordinatorEntity, SensorEntity, metaclass=ABCMeta):
+    """Abstract base class for other pollutant sensors from IRCELINE."""
 
-    _attr_has_entity_name = True
     _attr_attribution = ATTRIBUTION
+    _attr_has_entity_name = True
 
-    def __init__(
-        self, coordinator: IrcelineCoordinator, entry: ConfigEntry, pollutant: str
-    ) -> None:
+    def __init__(self, coordinator: IrcelineCoordinator, pollutant: str) -> None:
         """Set up the sensor."""
         super().__init__(coordinator)
         SensorEntity.__init__(self)
         self._attr_device_class = POLLUTANT_TO_SENSOR_DEVICE_CLASS[pollutant]
         self._attr_native_unit_of_measurement = POLLUTANT_TO_UNIT[pollutant]
-        self._attr_unique_id = f"{entry.entry_id}-{pollutant}"
-        self.entity_id = sensor.ENTITY_ID_FORMAT.format(
-            f"{str(entry.title).lower()}_{pollutant}"
-        )
         self._attr_device_info = coordinator.shared_device_info
         self._attr_translation_key = pollutant
         self._pollutant = pollutant
 
-    @property
-    def native_value(self) -> float | None:
-        """Get value of the sensor."""
-        return self._get_pollutant_data().get("value")
+    @abstractmethod
+    def _get_pollutant_data(self):
+        pass
 
     @property
     def extra_state_attributes(self) -> Mapping[str, Any] | None:
         """Return the timestamp of the data as additional attribute."""
         return {"timestamp": self._get_pollutant_data().get("timestamp")}
 
+    @property
+    def native_value(self) -> float | None:
+        """Get value of the sensor."""
+        return self._get_pollutant_data().get("value")
+
+
+class IrcelinePollutantRio(IrcelinePollutantSensor):
+    """Representation of a pollutant sensor with value extracted from the RIO interpolation of IRCELINE."""
+
+    def __init__(
+        self, coordinator: IrcelineCoordinator, entry: ConfigEntry, pollutant: str
+    ) -> None:
+        """Set up the sensor."""
+        super().__init__(coordinator, pollutant)
+        self._attr_unique_id = f"{entry.entry_id}-{pollutant}"
+        self.entity_id = sensor.ENTITY_ID_FORMAT.format(
+            f"{str(entry.title).lower()}_{pollutant}"
+        )
+
     def _get_pollutant_data(self) -> dict:
         return self.coordinator.data.get("rio", {}).get(
             POLLUTANT_TO_FEATURE[self._pollutant], {}
+        )
+
+
+class IrcelinePollutantForecast(IrcelinePollutantSensor):
+    """Representation of a pollutant sensor with value extracted from the daily forecast of IRCELINE."""
+
+    _attr_has_entity_name = True
+    _attr_attribution = ATTRIBUTION
+
+    def __init__(
+        self,
+        coordinator: IrcelineCoordinator,
+        entry: ConfigEntry,
+        pollutant: str,
+        time_delta: timedelta,
+    ) -> None:
+        """Set up the sensor."""
+        super().__init__(coordinator, pollutant)
+        self._attr_unique_id = f"{entry.entry_id}-{pollutant}_d{time_delta.days}"
+        self.entity_id = sensor.ENTITY_ID_FORMAT.format(
+            f"{str(entry.title).lower()}_{pollutant}_d{time_delta.days}"
+        )
+        self._time_delta = time_delta
+
+    def _get_pollutant_data(self) -> dict:
+        day = date.today() + self._time_delta
+        return self.coordinator.data.get("forecast", {}).get(
+            (POLLUTANT_TO_FEATURE[self._pollutant], day), {}
         )
