06e44867474a5186217454cca140af9a4511164a
Jules Dejaeghere <dej.j@live.be>
2024-06-27 18:29:06 +0200

    Create coordinator and refresh it first in __init__

--- homeassistant/components/irceline/__init__.py
+++ homeassistant/components/irceline/__init__.py
@@ -2,12 +2,13 @@
 
 from __future__ import annotations
 
-from open_irceline import IrcelineForecastClient, IrcelineRioClient
-
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import Platform
 from homeassistant.core import HomeAssistant
-from homeassistant.helpers.aiohttp_client import async_get_clientsession
+from homeassistant.helpers.update_coordinator import UpdateFailed
+
+from .const import DOMAIN
+from .coordinator import IrcelineCoordinator
 
 PLATFORMS: list[Platform] = [Platform.SENSOR]
 
@@ -17,18 +18,31 @@ type IrcelineConfigEntry = ConfigEntry  # noqa: F821
 async def async_setup_entry(hass: HomeAssistant, entry: IrcelineConfigEntry) -> bool:
     """Set up IRCEL - CELINE: Air quality Belgium from a config entry."""
 
-    session = async_get_clientsession(hass)
+    hass.data.setdefault(DOMAIN, {})
+    hass.data[DOMAIN][entry.entry_id] = irceline_coordinator = IrcelineCoordinator(
+        hass, entry
+    )
 
-    entry.runtime_data = {
-        "rio_client": IrcelineRioClient(session),
-        "forecast_client": IrcelineForecastClient(session),
-    }
+    try:
+        await irceline_coordinator.async_config_entry_first_refresh()
+    except UpdateFailed:
+        # This should be caught by the config flow anyway
+        return False
 
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
+    entry.async_on_unload(entry.add_update_listener(async_reload_entry))
 
     return True
 
 
 async def async_unload_entry(hass: HomeAssistant, entry: IrcelineConfigEntry) -> bool:
     """Unload a config entry."""
-    return await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
+    if unloaded := await hass.config_entries.async_unload_platforms(entry, PLATFORMS):
+        hass.data[DOMAIN].pop(entry.entry_id)
+    return unloaded
+
+
+async def async_reload_entry(hass: HomeAssistant, entry: ConfigEntry) -> None:
+    """Reload config entry."""
+    await async_unload_entry(hass, entry)
+    await async_setup_entry(hass, entry)
